db409929a765f110b4968342ff2e764d851facf6
jaridwarren <jaridwarren@gmail.com>
2018-08-12 22:24:17 -0700

    adding WP loops, set up blog-home and front-page properly

--- themes/inhabitent-theme/page-about.php
+++ themes/inhabitent-theme/page-about.php
@@ -4,4 +4,10 @@
 
 get_header(); ?>
 
+<div class="about-page">
+  <?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'template-parts/content', 'page' ); ?>
+	<?php endwhile; ?>
+</div>
+
 <?php get_footer(); ?>
