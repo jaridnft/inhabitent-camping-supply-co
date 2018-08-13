3e80b76a35094da240c49f3ad576fedf53f62b5f
jaridwarren <jaridwarren@gmail.com>
2018-08-12 21:17:35 -0700

    started functionality for header action on home page

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -5,6 +5,7 @@
 get_header(); ?>
 
   <div class="front-page-container">
+    <img class="main-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-full.svg"></img>
   </div>
 
 <?php get_footer(); ?>
--- themes/inhabitent-theme/functions.php
+++ themes/inhabitent-theme/functions.php
@@ -123,3 +123,7 @@ require get_template_directory() . '/inc/template-tags.php';
  * Custom functions that act independently of the theme templates.
  */
 require get_template_directory() . '/inc/extras.php';
+
+
+
+
