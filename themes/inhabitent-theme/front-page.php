e4b1a4fc94078d4675e7e6de73e1b080c1331092
jaridwarren <jaridwarren@gmail.com>
2018-08-12 22:55:52 -0700

    added about hero banner

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -4,8 +4,10 @@
 
 get_header(); ?>
 
-  <div class="front-page-container">
+
+  <div class="front-page-hero">
     <img class="main-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logos/inhabitent-logo-full.svg"></img>
   </div>
+
 
 <?php get_footer(); ?>
--- themes/inhabitent-theme/page-about.php
+++ themes/inhabitent-theme/page-about.php
@@ -4,10 +4,11 @@
 
 get_header(); ?>
 
-<div class="about-page">
+  <div class="about-page-hero">
+    <h1>About</h1>
+  </div>
   <?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', 'page' ); ?>
-	<?php endwhile; ?>
-</div>
+    <?php get_template_part( 'template-parts/content', 'page' ); ?>
+  <?php endwhile; ?>
 
 <?php get_footer(); ?>
