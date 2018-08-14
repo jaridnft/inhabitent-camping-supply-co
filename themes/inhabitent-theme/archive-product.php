ff9a3fcbfd74bb3541465bd9a2600b9303adc70b
jaridwarren <jaridwarren@gmail.com>
2018-08-14 16:52:23 -0700

    added product information, started styling shop page

--- themes/inhabitent-theme/archive-product.php
+++ themes/inhabitent-theme/archive-product.php
@@ -7,7 +7,7 @@ get_header(); ?>
 	<div id="primary" class="product-content-area">
 		<main id="main" class="product-site-main" role="main">
 			<h1>Shop Stuff</h1>
-			<h2><?php
+			<ul><?php
 				$terms = get_terms( array('taxonomy' => 'product_type') );
 				foreach ( $terms as $term ) {
 					$term_link = get_term_link( $term );
@@ -16,27 +16,29 @@ get_header(); ?>
   				if ( is_wp_error( $term_link ) ) {
 					continue;
 					}
-				echo '<a href="' . esc_url( $term_link ) . '">' . $term->name . '</a>';
+				echo '<li><a href="' . esc_url( $term_link ) . '">' . $term->name . '</a></li>';
 				echo ' ';
 				}?>
-			</h2>
+			</ul>
 
-		<?php if ( have_posts() ) : ?>
+		<div class="products-container">
+			<?php if ( have_posts() ) : ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					get_template_part( 'template-parts/content', 'product' );
-				?>
+					<?php
+						get_template_part( 'template-parts/content', 'product' );
+					?>
 
-			<?php endwhile; ?>
+				<?php endwhile; ?>
 
-		<?php else : ?>
+			<?php else : ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+				<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-		<?php endif; ?>
+			<?php endif; ?>
+		</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
