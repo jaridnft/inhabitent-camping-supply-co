f8d97f961dd002e0b8798ef4557c614dfc6393c5
jaridwarren <jaridwarren@gmail.com>
2018-08-16 14:08:24 -0700

    added more specificity to so only header search is targeted

--- themes/inhabitent-theme/js/header-search.js
+++ themes/inhabitent-theme/js/header-search.js
@@ -1,8 +1,12 @@
 (function($) {
-  $('.search-form').hide();
+  $('.main-navigation .search-form').hide();
   $('.main-navigation .fas.fa-search').click(function() {
-    $('.search-form').animate({ width: 'toggle' }, 350, function() {
-      $('.search-form input').focus();
-    });
+    $('.main-navigation .search-form').animate(
+      { width: 'toggle' },
+      350,
+      function() {
+        $('.main-navigation .search-form input').focus();
+      }
+    );
   });
 })(jQuery);
