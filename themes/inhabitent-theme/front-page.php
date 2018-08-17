314e36a358d4ad2dc7ca321f3e2a09b4228fc8c1
jaridwarren <jaridwarren@gmail.com>
2018-08-17 11:00:06 -0700

    adventures now populate on front-page.php

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -33,14 +33,14 @@ get_header(); ?>
     <h2>Inhabitent Journal</h2>
     <div class="front-page-journal-container">
         <?php		
-          $args = array(
-              'posts_per_page' => 3			
+          $journal_args = array(
+              'posts_per_page' => 3		
           );
 
-          $product_posts = get_posts( $args );
+          $journal_posts = get_posts( $journal_args );
         ?>
 
-        <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
+        <?php foreach ( $journal_posts as $post ) : setup_postdata( $post ); ?>
 
           <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
@@ -52,13 +52,13 @@ get_header(); ?>
 
               <?php if ( 'post' === get_post_type() ) : ?>
               
-              <div class="front-page-entry-meta">
-                <p><?php inhabitent_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?></p>
-                <?php the_title( sprintf( '<h3 class="front-page-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
-              </div><!-- .entry-meta -->
-              <?php 
-                  echo '<div class="read-more-container"><a href="'. esc_url( get_permalink() ) . '">Read more &rarr;</i></a></div>';
-              ?>
+                <div class="front-page-entry-meta">
+                  <p><?php inhabitent_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?></p>
+                  <?php the_title( sprintf( '<h3 class="front-page-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
+                </div><!-- .entry-meta -->
+                <?php 
+                    echo '<div class="journal-read-more-container"><a href="'. esc_url( get_permalink() ) . '">Read more &rarr;</i></a></div>';
+                ?>
 
               <?php endif; ?>
 
@@ -66,6 +66,43 @@ get_header(); ?>
 
         <?php endforeach; ?>
       </div> <!-- end of front-page-journal-container -->
+
+      <h2>Adventures</h2>
+    <div class="front-page-adventure-container">
+        <?php		
+          $adventure_args = array(
+              'posts_per_page' => 4,
+              'post_type' => 'adventure'		
+          );
+
+          $adventure_posts = get_posts( $adventure_args );
+        ?>
+
+        <?php foreach ( $adventure_posts as $post ) : setup_postdata( $post ); ?>
+
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+              <?php if ( has_post_thumbnail() ) : ?>
+
+                <?php the_post_thumbnail( 'large' ); ?>
+
+              <?php endif; ?>
+
+              <?php if ( 'adventure' === get_post_type() ) : ?>
+              
+                <?php the_title( sprintf( '<h3 class="front-page-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' );
+                  echo '<div class="adventure-read-more-container"><a href="'. esc_url( get_permalink() ) . '">Read more &rarr;</i></a></div>';
+                ?>
+
+              <?php endif; ?>
+
+          </article><!-- #post-## -->
+
+        <?php endforeach; ?>
+
+        <a class="more-adventures" href=#>More Adventures</a>
+      </div> <!-- end of front-page-adventure-container -->
+
     </div>
 
 <?php get_footer(); ?>
